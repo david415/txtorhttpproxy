1571e75106e766048b111587869fc3678c0efc41
David Stainton <dstainton415@gmail.com>
2014-11-02 22:28:35 +0000

    Add rough draft http to tor agent proxy

--- txtorproxyhttp/__init__.py
+++ txtorproxyhttp/__init__.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import
 
 from . import proxy
+from . import agent
 from .proxy import TorProxyFactory
+from .agent import TorAgent
 
-__all__ = ['proxy', 'TorProxyFactory']
+__all__ = ['proxy', 'agent', 'TorProxyFactory', 'TorAgent']
--- txtorproxyhttp/proxy.py
+++ txtorproxyhttp/proxy.py
@@ -1,19 +1,50 @@
 
-from twisted.web import proxy, http
-from twisted.internet import reactor
-from twisted.internet.endpoints import clientFromString
-from twisted.python import log
-
 import urlparse
 
+from twisted.web import proxy, http
+from twisted.internet import reactor, protocol, defer
+from twisted.internet.endpoints import clientFromString
+from twisted.python import log
+from twisted.web.client import PartialDownloadError
+from twisted.web.http import PotentialDataLoss
+from twisted.web._newclient import ResponseDone
 
-class TorProxyRequest(proxy.Request):
+from .agent import TorAgent
+
+# yay debugging
+#defer.setDebugging(True)
+
+class ProxyBodyProtocol(protocol.Protocol):
+
+    def __init__(self, request):
+        log.msg("proxybodyprotocol init")
+        self.request = request
+
+    def dataReceived(self, data):
+        log.msg(data)
+        self.request.write(data)
+
+    def connectionLost(self, reason):
+        """
+        Deliver the accumulated response bytes to the waiting L{Deferred}, if
+        the response body has been completely received without error.
+        """
+        log.msg("connection lost")
+        if reason.check(ResponseDone):
+            self.request.finish()
+
+        elif reason.check(PotentialDataLoss):
+            log.err("connectionLost")
+            log.err(reason)
+
+
+class TorProxyRequest(http.Request):
     """copied from twisted.web.proxy.ProxyRequest... and modified"""
-    protocols = {'http': proxy.ProxyClientFactory}
+
     ports = {'http': 80}
 
     def __init__(self, channel, queued, reactor=reactor, socksPort=None):
-        proxy.Request.__init__(self, channel, queued)
+        http.Request.__init__(self, channel, queued)
         self.reactor = reactor
         self.socksPort = socksPort
 
@@ -21,37 +52,41 @@ class TorProxyRequest(proxy.Request):
 
         log.msg("Request %s from %s" % (self.uri, self.getClientIP()))
 
-        parsed = urlparse.urlparse(self.uri)
-        protocol = parsed[0]
-        host = parsed[1]
-        port = self.ports[protocol]
-        if ':' in host:
-            host, port = host.split(':')
-            port = int(port)
-        rest = urlparse.urlunparse(('', '') + parsed[2:])
-        if not rest:
-            rest = rest + '/'
-        class_ = self.protocols[protocol]
         headers = self.getAllHeaders().copy()
         if 'host' not in headers:
             headers['host'] = host
+
+        # XXX
+        #self.content.seek(0, 0)
+        #s = self.content.read()
+
+        log.msg("URI %s" % self.uri)
+
         self.content.seek(0, 0)
         s = self.content.read()
-        clientFactory = class_(self.method, rest, self.clientproto, headers,
-                               s, self)
-        # XXX
-        if self.socksPort:
-            self.endpointDescriptor = "tor:host=%s:port=%s:socksPort=%s" % (host, port, self.socksPort)
-        else:
-            self.endpointDescriptor = "tor:host=%s:port=%s" % (host, port)
-        torEndpoint = clientFromString(self.reactor, self.endpointDescriptor)
-        d = torEndpoint.connect(clientFactory)
 
-        @d.addErrback
-        def _gotError(error):
-            # XXX
-            log.err(error)
-            log.err("TorProxyRequest: failure to connect: %s" % self.endpointDescriptor)
+        log.msg("request content %s" % s)
+
+        agent = TorAgent(reactor)
+        d = agent.request(self.method, self.uri, self.requestHeaders, None)
+
+        def agentCallback(response):
+            log.msg(response)
+            self.setResponseCode(response.code, response.phrase)
+
+            for name, values in response.headers.getAllRawHeaders():
+                log.msg("YOOYO name %s values %s" % (name, values))
+                self.responseHeaders.setRawHeaders(name, values)
+
+            log.msg("before deliverBody")
+            response.deliverBody(ProxyBodyProtocol(self))
+            log.msg("after deliverBody")
+
+        def agentErrback(failure):
+            log.err(failure)
+            return failure
+
+        d.addCallbacks(agentCallback, agentErrback)
 
 
 class TorProxy(http.HTTPChannel):
@@ -80,5 +115,3 @@ class TorProxyFactory(http.HTTPFactory):
 
     def buildProtocol(self, addr):
         return TorProxy(socksPort=self.socksPort)
-
-
