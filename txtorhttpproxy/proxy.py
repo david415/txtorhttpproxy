cad43caf02b6e763ef4319bf47ae6d0acc2b3a27
David Stainton <dstainton415@gmail.com>
2014-11-03 13:20:30 +0000

    Add docstrings to agent proxy classes

--- txtorhttpproxy/proxy.py
+++ txtorhttpproxy/proxy.py
@@ -7,14 +7,30 @@ from twisted.web._newclient import ResponseDone
 
 
 class ProxyBodyProtocol(protocol.Protocol):
+    """
+    Protocol that proxies data sent to it.
 
+    This is a helper for L{AgentProxyRequest}, which proxies the body and
+    notifies the requesting AgentProxyRequest instance when data proxying
+    is finished.
+    """
     def __init__(self, request):
+        """
+        @param request: An instance of L{AgentProxyRequest}
+        """
         self.request = request
 
     def dataReceived(self, data):
+        """
+        Immediately write the received that to our companion
+        request class, an instance of L{AgentProxyRequest}
+        """
         self.request.write(data)
 
     def connectionLost(self, reason):
+        """
+        Notify our companion request that data proxying is finished.
+        """
         # XXX
         # do we care why a connection was lost?
         # i should probably rewrite this method
@@ -30,15 +46,29 @@ class ProxyBodyProtocol(protocol.Protocol):
 
 
 class AgentProxyRequest(http.Request):
+    """
+    A HTTP Request that proxies.
 
+    This class uses the ProxyBodyProtocol helper class to retrieve
+    the response data.
+    """
     ports = {'http': 80}
 
-    def __init__(self, channel, queued, reactor=reactor, socksPort=None):
+    def __init__(self, channel, queued):
+        """
+        @param channel: the channel we're connected to.
+        @param queued: are we in the request queue, or can we start writing to
+            the transport?
+        """
         http.Request.__init__(self, channel, queued)
-        self.reactor = reactor
-        self.socksPort = socksPort
 
     def process(self):
+        """
+        Our parent class calls this method when our helper class
+        ProxyBodyProtocol has signaled that the data retrieval is finished.
+        We then set our response attributes such as response code, phrase
+        and then send the data to the requestor.
+        """
 
         log.msg("AgentProxyRequest: requested uri %s from %s" % (self.uri, self.getClientIP()))
 
@@ -63,6 +93,10 @@ class AgentProxyRequest(http.Request):
 
 
 class AgentProxy(http.HTTPChannel):
+    """
+    A HTTP proxy which uses the helper class AgentProxyRequest
+    to proxy outgoing requests.
+    """
     requestFactory = AgentProxyRequest
 
     # XXX can we get rid of these class attributes and make
@@ -75,11 +109,24 @@ class AgentProxy(http.HTTPChannel):
     __content = None
 
     def __init__(self, agent):
+        """
+        Create an L{AgentProxy}.
+
+        @param agent: an IAgent instance used by our helper class AgentProxyRequest
+            to send outbound HTTP requests
+        """
         self.requestFactory.agent = agent
         http.HTTPChannel.__init__(self)
 
 
 class AgentProxyFactory(http.HTTPFactory):
+    """
+    A factory for L{AgentProxy}, used by applications wishing to proxy
+    incoming HTTP requests.
+
+    @ivar agent: The IAgent instance used to make outbound HTTP requests
+    on behalf of the HTTP client using this proxy
+    """
 
     def __init__(self, agent):
         self.agent = agent
